28b32551e1ddae6697597847b5081b5098d73e8b
anhtuanutc <tuan11a4dl@gmail.com>
2022-10-26 00:13:52 +0700

    Update Front_End Admin
    --author:Lợn Cọc
    Date:12:13Am 26/10/2022

---
+++ Front_End/webtintuc/src/components/Admin/Admin.vue
@@ -0,0 +1,32 @@
+<template>
+    <LoginAdmin @DangNhapvaoAdmin="show" v-if="!Check_login"/>
+    <HomeAdmin  v-if="Check_login"
+    />
+</template>
+
+<script>
+import HomeAdmin from '../Admin/Home_Admin.vue'
+import LoginAdmin from '../Admin/Login.vue'
+
+export default {
+  name: 'Admin',
+  components: {
+    HomeAdmin,LoginAdmin
+  },
+  data() {
+    return {
+        Check_login: false,
+    };
+  },
+  methods: {
+    show(login){
+        try {
+            this.Check_login=login;
+        } catch (error) {
+            console.log(error);
+        }
+        
+    }
+  },
+}
+</script>
---
+++ Front_End/webtintuc/src/components/Admin/Home_Admin.vue
@@ -0,0 +1,22 @@
+<template>
+    <Header/>
+    <Nav/>
+    <Content/>
+</template>
+<script>
+import Header from '../Admin/Layout/Header.vue'
+import Nav from '../Admin/Layout/Nav.vue'
+import Content from '../Admin/Layout/Content.vue'
+export default {
+  name: 'Admin',
+  components: {
+    Header,Nav,Content
+  }
+}
+</script>
+<style scoped>
+@import url(../../assets/css/bootstrap.min.css);
+@import url(../../assets/css/font-awesome.min.css);
+@import url(../../assets/css/datepicker3.css);
+@import url(../../assets/css/styles.css);
+</style>
